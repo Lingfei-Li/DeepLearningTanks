510640e6afe56eae034612086c92583fd239d340
Lingfei Li <cslilingfei@outlook.com>
2017-02-07 21:17:48 -0600

    Adding features

--- featureExtractors2.py
+++ featureExtractors2.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-#
+# 
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -101,30 +101,40 @@ class SimpleExtractor(FeatureExtractor):
     def probBullet(self, state, action):
         bullet_prob = 0.0
         player = state.player
-        next_x, next_y = self.calcNextPos(player.rect, action - 1, 10.0)
+        next_x, next_y = self.calcNextPos(player.rect, action-1, 10.0)
 
         factor = 100
         for bullet in state.bullets:
             dist = math.hypot(bullet.rect.centerx - next_x, bullet.rect.centery - next_y)
             if dist is not None:
                 if abs(bullet.rect.centery - next_y) < 50:
-                    if (
-                            player.direction == Const.DIR_UP or player.direction == Const.DO_DOWN) and player.direction == action - 1:
-                        bullet_prob += 0.1 * factor / dist
-                    else:
-                        bullet_prob += factor / dist
-                elif abs(bullet.rect.centerx - next_x) < 50:
-                    if (
-                            player.direction == Const.DIR_LEFT or player.direction == Const.DO_RIGHT) and player.direction == action - 1:
-                        bullet_prob += 0.1 * factor / dist
-                    else:
-                        bullet_prob += factor / dist
+                    if bullet.rect.centerx < next_x and bullet.direction == Const.DIR_RIGHT:
+                        if (player.direction == Const.DIR_UP or player.direction == Const.DO_DOWN) and player.direction == action-1:
+                            bullet_prob += 0.1*factor/dist
+                        else:
+                            bullet_prob += factor/dist
+                    elif bullet.rect.centerx > next_x and bullet.direction == Const.DIR_LEFT:
+                        if (player.direction == Const.DIR_UP or player.direction == Const.DO_DOWN) and player.direction == action-1:
+                            bullet_prob += 0.1*factor/dist
+                        else:
+                            bullet_prob += factor/dist
+                elif abs(bullet.rect.centerx-next_x) < 50:
+                    if bullet.rect.centery < next_y and bullet.direction == Const.DIR_DOWN:
+                        if (player.direction == Const.DIR_LEFT or player.direction == Const.DO_RIGHT) and player.direction == action-1:
+                            bullet_prob += 0.1*factor/dist
+                        else:
+                            bullet_prob += factor/dist
+                    elif bullet.rect.centery > next_y and bullet.direction == Const.DIR_UP:
+                        if (player.direction == Const.DIR_LEFT or player.direction == Const.DO_RIGHT) and player.direction == action-1:
+                            bullet_prob += 0.1*factor/dist
+                        else:
+                            bullet_prob += factor/dist
 
-                return bullet_prob / factor
+        return bullet_prob/factor
 
     def probEnemy(self, state, action):
         player = state.player
-        next_x, next_y = self.calcNextPos(player.rect, action - 1, 10.0)
+        next_x, next_y = self.calcNextPos(player.rect, action-1, 10.0)
 
         enemy_prob = 0.0
         factor = 100
@@ -133,33 +143,44 @@ class SimpleExtractor(FeatureExtractor):
             dist = math.hypot(enemy.rect.centerx - next_x, enemy.rect.centery - next_y)
             if dist is not None:
                 if abs(enemy.rect.centery - next_y) < 50:
-                    if (
-                            player.direction == Const.DIR_UP or player.direction == Const.DO_DOWN) and player.direction == action - 1:
-                        enemy_prob += 0.1 * factor / dist
+                    if enemy.rect.centerx < next_x and enemy.direction == Const.DIR_RIGHT:
+                        if (player.direction == Const.DIR_UP or player.direction == Const.DO_DOWN) and player.direction == action-1:
+                            enemy_prob += 0.1*factor/dist
+                        else:
+                            enemy_prob += factor/dist
+                    elif enemy.rect.centerx > next_x and enemy.direction == Const.DIR_LEFT:
+                        if (player.direction == Const.DIR_UP or player.direction == Const.DO_DOWN) and player.direction == action-1:
+                            enemy_prob += 0.1*factor/dist
+                        else:
+                            enemy_prob += factor/dist
                     else:
-                        enemy_prob += factor / dist
+                        enemy_prob += 0.5*factor/dist
                 elif abs(enemy.rect.centerx - next_x) < 50:
-                    if (
-                            player.direction == Const.DIR_LEFT or player.direction == Const.DO_RIGHT) and player.direction == action - 1:
-                        enemy_prob += 0.1 * factor / dist
+                    if enemy.rect.centery < next_y and enemy.direction == Const.DIR_DOWN:
+                        if (player.direction == Const.DIR_LEFT or player.direction == Const.DO_RIGHT) and player.direction == action-1:
+                            enemy_prob += 0.1*factor/dist
+                        else:
+                            enemy_prob += factor/dist
+                    elif enemy.rect.centery > next_y and enemy.direction == Const.DIR_UP:
+                        if (player.direction == Const.DIR_LEFT or player.direction == Const.DO_RIGHT) and player.direction == action-1:
+                            enemy_prob += 0.1*factor/dist
+                        else:
+                            enemy_prob += factor/dist
                     else:
-                        enemy_prob += factor / dist
+                        enemy_prob += 0.5*factor/dist
 
-        return enemy_prob / factor
+        return enemy_prob/factor
 
-    def verticalSpace(self, state, action):
+    def probSpace(self, state, action):
         player = state.player
-        next_x, next_y = self.calcNextPos(player.rect, action - 1, 10.0)
+        x = player.rect.centerx
+        y = player.rect.centery
+        dx, dy = self.calcPosDiff(action-1, 10.0)
+        next_x, next_y = float(x + dx), float(y + dy)
 
-        return abs(abs(next_y) - abs(416 - next_y))
-
-
-    def horizontalSpace(self, state, action):
-        player = state.player
-        next_x, next_y = self.calcNextPos(player.rect, action - 1, 10.0)
+        return abs(abs(next_x)-abs(480-next_x)) + abs(abs(next_y)-abs(416-next_y))
 
 
-        return abs(abs(next_x) - abs(480 - next_x))
 
 
     def probHitEnemy(self, state, action):
@@ -168,7 +189,7 @@ class SimpleExtractor(FeatureExtractor):
         for enemy in facedEnemies:
             dist = util.dist(state.player, enemy)
             if dist is not None:
-                prob += 120 / dist
+                prob += 120/dist
                 # prob += 480/abs(enemy.rect.centerx - state.player.rect.centerx)
                 # prob += 416/abs(enemy.rect.centery - state.player.rect.centery)
         return prob
@@ -182,9 +203,7 @@ class SimpleExtractor(FeatureExtractor):
         # features["hit"] = self.probHitEnemy(state, action)
         features["enemy"] = self.probEnemy(state, action)
         features["bullet"] = self.probBullet(state, action)
-        features["vertSpace"] = self.verticalSpace(state, action)
-        features["horizonSpace"] = self.horizontalSpace(state, action)
-
+        # features["space"] = self.probSpace(state, action)
 
         features.divideAll(2000.0)
         return features
